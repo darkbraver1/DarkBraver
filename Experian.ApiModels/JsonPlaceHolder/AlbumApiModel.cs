b8abff12aab7fa39cd5cdf0c76a61f3e7aab61ec
darkbraver1 <90731565+darkbraver1@users.noreply.github.com>
2021-09-20 12:17:32 +0100

    Api Models via manual upload

---
+++ Experian.ApiModels/JsonPlaceHolder/AlbumApiModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Experian.ApiModels.JsonPlaceHolder
+{
+    public class AlbumApiModel
+    {
+        public int UserId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Experian.ApiModels/JsonPlaceHolder/PhotoApiModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Experian.ApiModels.JsonPlaceHolder
+{
+    public class PhotoApiModel
+    {
+        public int AlbumId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string ThumbnailUrl { get; set; }
+    }
+}
