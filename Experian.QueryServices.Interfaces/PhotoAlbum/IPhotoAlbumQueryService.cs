86351a5b60fe1ddc63ac526462956a24edd67905
darkbraver1 <90731565+darkbraver1@users.noreply.github.com>
2021-09-20 12:28:50 +0100

    Add files via upload

---
+++ Experian.QueryServices.Interfaces/PhotoAlbum/IPhotoAlbumQueryService.cs
@@ -0,0 +1,14 @@
+﻿using Experian.ApiModels.JsonPlaceHolder;
+using Experian.QueryModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Experian.QueryServices.Interfaces.PhotoAlbum
+{
+    public interface IPhotoAlbumQueryService
+    {
+        IEnumerable<PhotoAlbumQueryModel> GetPhotoAlbums(IEnumerable<AlbumApiModel> albums, IEnumerable<PhotoApiModel> photos);
+
+        IEnumerable<PhotoAlbumQueryModel> GetPhotoAlbumByUserId(IEnumerable<AlbumApiModel> albums, IEnumerable<PhotoApiModel> photos, int userId);
+    }
+}
