bd00c1569af038a175e676b7dd04e40624baa7a5
darkbraver1 <90731565+darkbraver1@users.noreply.github.com>
2021-09-20 12:26:49 +0100

    Add files via upload

---
+++ Experian.QueryModels/PhotoAlbumQueryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Experian.QueryModels
+{
+    public class PhotoAlbumQueryModel
+    {
+        public int UserId { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public string AlbumTitle { get; set; }
+
+        public int PhotoId { get; set; }
+
+        public string PhotoTitle { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public string PhotoThumbnailUrl { get; set; }
+    }
+}
